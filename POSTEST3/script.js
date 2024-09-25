ed097c3755d37ba18d9a9fe9d47eaf432fa7a468
NaylaZeanlita <148698235+NaylaZeanlita@users.noreply.github.com>
2024-09-25 16:37:57 +0800

    Add files via upload

---
+++ POSTEST3/script.js
@@ -0,0 +1,10 @@
+function welcome(name) {
+    var name = prompt("What's your name?");
+    console.log(name);
+    return "Welcome " + name + ", to the road construction project monitoring website, we provide information on project developments that can be accessed publicly.";
+}
+
+function displayWelcome() {
+    var message = welcome();
+    document.getElementById('welcomeUser').textContent = message;
+}
